9a261b4bb5647c3a6270e01a84a8525c5185b85a
Jason Lin <linjay@umich.edu>
2024-08-01 12:51:04 -0400

    setup

---
+++ marketingMaturity/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+    <Route path="/" element = {<Landing/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ marketingMaturity/src/pages/landingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Landing(){
+const navigate = useNavigate()
+return (
+
+)
+}
+
+
