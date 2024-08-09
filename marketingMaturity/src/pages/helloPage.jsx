6b86292d5671f96079f07d2b87e651ba471f463d
Jason Lin <linjay@umich.edu>
2024-08-09 12:00:35 -0400

    changed blur when dialog is open

--- marketingMaturity/src/pages/helloPage.jsx
+++ marketingMaturity/src/pages/helloPage.jsx
@@ -10,39 +10,38 @@ import NextButton from '../components/nextButton';
 function HelloPage() {
     const navigate = useNavigate();
     const [showOverlay, setShowOverlay] = useState(true);
+
     const handleOverlay = () => {
         setShowOverlay(false);
     };
+
     return (
         <div className='helloContainer'>
             {showOverlay && (
-                <>
-                    <div className="overlay" />
-                    <dialog className="welcomeDialog" open>
-                        <span style={{ fontWeight: 'bold' }}>
-                            Survey Disclaimer
-                        </span>
-                        <p>
-                            Thank you for taking the time to participate in our survey. We want to assure you that your responses will be used exclusively for general analytical purposes. No personal information that could identify you individually will be collected. Your privacy is important to us, and we are committed to maintaining the confidentiality of your responses.
-                        </p>
-                        <form method="dialog">
-                            <div className="dialogButtonContainer">
-                                <BasicRoundButton onClick={handleOverlay}>Start Survey</BasicRoundButton>
-                            </div>
-                        </form>
-                    </dialog>
-                </>
+                <dialog className="welcomeDialog" open>
+                    <span style={{ fontWeight: 'bold' }}>
+                        Survey Disclaimer
+                    </span>
+                    <p>
+                        Thank you for taking the time to participate in our survey. We want to assure you that your responses will be used exclusively for general analytical purposes. No personal information that could identify you individually will be collected. Your privacy is important to us, and we are committed to maintaining the confidentiality of your responses.
+                    </p>
+                    <form method="dialog">
+                        <div className="dialogButtonContainer">
+                            <BasicRoundButton onClick={handleOverlay}>Start Survey</BasicRoundButton>
+                        </div>
+                    </form>
+                </dialog>
             )}
-            <form className='form'>
+            <form className={`form ${showOverlay ? 'blurred' : ''}`}>
                 <h1 className='header'>Hello!</h1>
                 <p className='instructions'>
                     Please fill out the following fields with accurate information to help us better understand your marketing performance. All fields are required.
                 </p>
-                <CountrySelect/>
-                <PartnerLevelSelect/>
-                <YesNoGroup/>
+                <CountrySelect />
+                <PartnerLevelSelect />
+                <YesNoGroup />
                 <div className='nextButtonContainer'>
-                    <NextButton onClick={() => navigate('/BGSB')}/>
+                    <NextButton onClick={() => navigate('/BGSB')} />
                 </div>
             </form>
         </div>
